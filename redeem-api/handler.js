2f9a48981fb227c4f2501c022592e7a9cbec4b06
turinglabs <sebastiano.cataudo@gmail.com>
2022-01-26 06:38:03 +0100

    adding event claim initial feature

--- redeem-api/handler.js
+++ redeem-api/handler.js
@@ -4,7 +4,7 @@ const serverless = require("serverless-http");
 const mailgun = require("mailgun-js");
 const web3 = require("web3");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const cors = require('cors')
+const cors = require('cors');
 
 const USERS_TABLE = process.env.USERS_TABLE;
 const dynamoDbClientParams = {};
@@ -32,6 +32,108 @@ function createRedemptionCode(length) {
       charactersLength));
   }
   return result;
+}
+
+function createNameVerificationRequest(req) {
+  return new Promise(async response => {
+    try {
+      const { Item } = await dynamoDbClient.get({
+        TableName: USERS_TABLE,
+        Key: {
+          userId: req.params.userId,
+        },
+      }).promise();
+      if (Item && !Item.verified) {
+        const redemptionCode = createRedemptionCode(24)
+        dynamoDbClient.update({
+          TableName: USERS_TABLE,
+          Key: {
+            userId: req.params.userId,
+          },
+          UpdateExpression: "set secret = :s",
+          ExpressionAttributeValues: {
+            ":s": redemptionCode
+          },
+          ReturnValues: "UPDATED_NEW"
+        }, function (err, data) {
+          if (err) {
+            res.json({ message: "Something goes wrong, please retry." })
+          } else {
+            mg.messages().send({
+              from: 'PolygonME Verification BOT <noreply@' + DOMAIN + '>',
+              to: Item.email,
+              subject: 'Verify the ownership of your e-mail.',
+              html: 'Thank you, this your redemption code: ' + redemptionCode
+            }, function (error, body) {
+              if (error) {
+                response({ message: "Something goes wrong, please reply." });
+              } else {
+                response({ message: "Please verify your e-mail now." });
+              }
+            })
+          }
+        });
+      } else if (Item.verified) {
+        response({ error: 'This account have been verified yet' });
+      } else {
+        response({ error: 'Could not find user with provided "userId"' });
+      }
+    } catch (error) {
+      console.log(error);
+      response({ error: "Could not ask verification" });
+    }
+  })
+}
+
+function createEventVerificationRequest(req) {
+  return new Promise(async response => {
+    try {
+      const { Item } = await dynamoDbClient.get({
+        TableName: USERS_TABLE,
+        Key: {
+          eventId: req.params.eventId,
+        },
+      }).promise();
+      if (Item && !Item.verified) {
+        const redemptionCode = createRedemptionCode(24)
+        dynamoDbClient.update({
+          TableName: USERS_TABLE,
+          Key: {
+            eventId: req.params.eventId,
+          },
+          UpdateExpression: "set secret = :s",
+          ExpressionAttributeValues: {
+            ":s": redemptionCode
+          },
+          ReturnValues: "UPDATED_NEW"
+        }, function (err, data) {
+          if (err) {
+            res.json({ message: "Something goes wrong, please retry." })
+          } else {
+            mg.messages().send({
+              from: 'PolygonME Verification BOT <noreply@' + DOMAIN + '>',
+              to: Item.email,
+              subject: 'Verify the ownership of your e-mail.',
+              html: 'Thank you, this your redemption code: ' + redemptionCode
+            }, function (error, body) {
+              if (error) {
+                response({ message: "Something goes wrong, please reply." });
+              } else {
+                response({ message: "Please verify your e-mail now." });
+              }
+            })
+          }
+        });
+      } else if (Item.verified) {
+        response({ error: 'This account have been verified yet' });
+      } else {
+        response({ error: 'Could not find user with provided "userId"' });
+      }
+    } catch (error) {
+      console.log(error);
+      response({ error: "Could not ask verification" });
+    }
+  })
 }
 
 app.get("/users/:userId", async function (req, res) {
@@ -62,11 +164,11 @@ app.get("/users/:userId", async function (req, res) {
   }
 });
 
-app.post("/users", async function (req, res) {
+app.post("/user", async function (req, res) {
   const { userId, email, secret } = req.body;
   if (secret === SECRET) {
-    if (typeof userId !== "string") {
-      res.status(400).json({ error: '"userId" must be a string' });
+    if (userId === undefined) {
+      res.status(400).json({ error: 'Malformed request' });
     } else if (typeof email !== "string") {
       res.status(400).json({ error: '"name" must be a string' });
     }
@@ -94,59 +196,98 @@ app.post("/users", async function (req, res) {
   }
 });
 
-app.get("/ask/:userId", async function (req, res) {
+app.post("/event", async function (req, res) {
+  const { eventId, email, secret } = req.body;
+  if (secret === SECRET) {
+    if (eventId === undefined) {
+      res.status(400).json({ error: 'Malformed request' });
+    } else if (typeof email !== "string") {
+      res.status(400).json({ error: '"name" must be a string' });
+    }
+    try {
+      await dynamoDbClient.put({
+        TableName: USERS_TABLE,
+        Item: {
+          userId: eventId,
+          email: email,
+          verified: false,
+          address: "",
+          signature: '',
+          redeemed: false,
+          secret: ""
+        },
+      }).promise();
+      res.json({ message: "Event added correctly.", error: false });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Could not create event" });
+    }
+  } else {
+    res.status(401).json({ error: "Not authorized" })
+  }
+});
+
+app.get("/ask/name/:userId", async function (req, res) {
+  const response = await createNameVerificationRequest(req)
+  res.json(response)
+});
+
+app.get("/ask/event/:eventId", async function (req, res) {
+  const response = await createEventVerificationRequest(req)
+  res.json(response)
+});
+
+// Claim event
+app.post("/claim/:eventId", async function (req, res) {
   try {
     const { Item } = await dynamoDbClient.get({
       TableName: USERS_TABLE,
       Key: {
-        userId: req.params.userId,
+        eventId: req.params.eventId,
       },
     }).promise();
-    if (Item && !Item.verified) {
-      const redemptionCode = createRedemptionCode(24)
-      dynamoDbClient.update({
-        TableName: USERS_TABLE,
-        Key: {
-          userId: req.params.userId,
-        },
-        UpdateExpression: "set secret = :s",
-        ExpressionAttributeValues: {
-          ":s": redemptionCode
-        },
-        ReturnValues: "UPDATED_NEW"
-      }, function (err, data) {
-        if (err) {
-          res.json({ message: "Something goes wrong, please retry." })
-        } else {
-          mg.messages().send({
-            from: 'PolygonME Verification BOT <noreply@' + DOMAIN + '>',
-            to: Item.email,
-            subject: 'Verify the ownership of your e-mail.',
-            html: 'Thank you, this your redemption code: ' + redemptionCode
-          }, function (error, body) {
-            if (error) {
-              res.status(200).json({ message: "Something goes wrong, please reply." });
-            } else {
-              res.status(200).json({ message: "Please verify your e-mail now." });
-            }
-          })
-        }
-      });
-    } else if (Item.verified) {
-      res
-        .status(200)
-        .json({ error: 'This account have been verified yet' });
+    if (Item) {
+      const provider = new HDWalletProvider(
+        process.env.DUMMY_MNEMONIC,
+        process.env.POLYGON_PROVIDER
+      );
+      const web3Instance = new web3(provider);
+      const verified = await web3Instance.eth.personal.ecRecover(req.body.secret, req.body.signature)
+      if (verified.toUpperCase() === req.body.address.toUpperCase()) {
+        dynamoDbClient.update({
+          TableName: USERS_TABLE,
+          Key: {
+            eventId: req.params.eventId,
+          },
+          UpdateExpression: "set verified = :v, address = :a, signature = :s",
+          ExpressionAttributeValues: {
+            ":v": true,
+            ":a": req.body.address,
+            ":s": req.body.signature
+          },
+          ReturnValues: "UPDATED_NEW"
+        }, function (err, data) {
+          if (err) {
+            res.status(500).json({ message: "Something goes wrong, please retry." })
+          } else {
+            // TODO: Send the NFT to the user
+          }
+        });
+      } else {
+        res.status(200).json({ error: "Can't verify your signature, please send valid data." });
+      }
     } else {
       res
         .status(404)
         .json({ error: 'Could not find user with provuserIded "userId"' });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not ask verification" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Something goes wrong, please retry" });
   }
 });
 
+// Verify userId
 app.post("/verify/:userId", async function (req, res) {
   try {
     const { Item } = await dynamoDbClient.get({
