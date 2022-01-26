4fa3841da2a71ade69159d40284855f1134e4914
turinglabs <sebastiano.cataudo@gmail.com>
2022-01-26 13:22:03 +0100

    adding admin filter

--- redeem-api/handler.js
+++ redeem-api/handler.js
@@ -256,20 +256,30 @@ app.post("/event", async function (req, res) {
       res.status(400).json({ error: '"name" must be a string' });
     }
     try {
-      await dynamoDbClient.put({
+      const { Item } = await dynamoDbClient.get({
         TableName: USERS_TABLE,
-        Item: {
+        Key: {
           userId: tokenId + "-" + email,
-          isEvent: true,
-          email: email,
-          verified: false,
-          address: "",
-          signature: '',
-          redeemed: false,
-          secret: ""
         },
       }).promise();
-      res.json({ message: "Event added correctly.", error: false });
+      if (!Item) {
+        await dynamoDbClient.put({
+          TableName: USERS_TABLE,
+          Item: {
+            userId: tokenId + "-" + email,
+            isEvent: true,
+            email: email,
+            verified: false,
+            address: "",
+            signature: '',
+            redeemed: false,
+            secret: ""
+          },
+        }).promise();
+        res.json({ message: "Event added correctly.", error: false });
+      } else {
+        res.json({ message: "Event exists, can't add again.", error: true });
+      }
     } catch (error) {
       console.log(error);
       res.status(500).json({ error: "Could not create event" });
